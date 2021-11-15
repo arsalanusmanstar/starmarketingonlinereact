808b4e444f3246d5098ce8cd6e7ee52c1b3bc144
arsalanusmanstar <92919937+arsalanusmanstar@users.noreply.github.com>
2021-11-15 20:06:45 +0500

    add some changes

--- src/components/elements/meta.js
+++ src/components/elements/meta.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import MetaTags from 'react-meta-tags';
+import {Helmet} from "react-helmet";
 
 const Meta = ({meta}) => {
     return (
         <div className="wrapper">
-           {meta && <MetaTags>
+           {meta && <Helmet>
+                <meta charSet="utf-8" />
             {meta[3] && <title>{meta[3].content}</title>}
             {meta[4] && <meta name="description" content={meta[4].content} />}
             {meta[5] && <meta property="og:title" content={meta[5].content} />}
-          </MetaTags> }
+          </Helmet> }
         </div>
       )
   }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-    </BrowserRouter>, 
-  document.getElementById('root')
-);
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(
+    <BrowserRouter>
+      <App />
+      </BrowserRouter>, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
